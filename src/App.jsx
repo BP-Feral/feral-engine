c60bdd9421e626ff538597da7dfcc7ecea69025b
BlackPigeon <mihai.pricob@yahoo.com>
2025-09-24 02:48:55 +0300

    🔨 Streamline Engine page access

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './pages/HomePage'
-import ProjectOne from './pages/ProjectOne'
-import ProjectTwo from './pages/ProjectTwo'
 import FeralEngine from './pages/FeralEngine'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/projects" element = {<HomePage/>}></Route>
-        <Route path="/projects/project-one" element = {<ProjectOne/>}></Route>
-        <Route path="/projects/project-two" element = {<ProjectTwo/>}></Route>
-        <Route path="/projects/feral-engine" element={<FeralEngine/>}></Route>
-      </Routes>
-    </Router>
+    <FeralEngine/>
   )
 }
 
